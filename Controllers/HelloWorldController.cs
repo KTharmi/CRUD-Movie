814250288fb9a7242bf4b7ca023734429142bafc
KTharmi <tharmikulendiran5@gmail.com>
2020-01-27 06:45:37 -0800

    first commit

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        /*public string Index()
+        {
+            return "This is my default action...";
+        }*/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+
+        // GET: /HelloWorld/Welcome/ 
+        /*public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }*/
+        // Requires using System.Text.Encodings.Web;
+        /*public string Welcome(string name, string address, int numTimes = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name},{address}, NumTimes is: {numTimes}");
+        }*/
+
+        /*public string Welcome(string name, int ID = 1)
+        {
+            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+        }*/
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required] 
+        public string Title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+
+        
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+        
+        public string Rating { get; set; }
+    }
+}
